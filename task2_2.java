5f1f2f820df53e38a997f0b91ec46d4315480edd
Leonid Maslov <mlenkei@mail.ru>
2023-06-22 04:24:16 +0300

    task_interim_certification

--- task2_2.java
+++ task2_2.java
@@ -1,64 +1,56 @@
 // Узел связанного списка
-class Node
-{
+class Node {
     int data;
     Node next;
- 
-    Node(int data, Node next)
-    {
+
+    Node(int data, Node next) {
         this.data = data;
         this.next = next;
     }
 }
- 
-class Main
-{
+
+class Main {
     // Вспомогательная функция для печати заданного связанного списка
-    public static void printList(Node head)
-    {
+    public static void printList(Node head) {
         Node ptr = head;
-        while (ptr != null)
-        {
+        while (ptr != null) {
             System.out.print(ptr.data + " —> ");
             ptr = ptr.next;
         }
- 
+
         System.out.println("null");
     }
- 
+
     // Переворачивает заданный связанный список, изменяя его поля `.next` и
     // его голова.
-    public static Node reverse(Node head)
-    {
+    public static Node reverse(Node head) {
         Node previous = null;
         Node current = head;
- 
+
         // обход списка
-        while (current != null)
-        {
+        while (current != null) {
             // сложно: обратите внимание на следующий узел
             Node next = current.next;
- 
-            current.next = previous;    // фиксируем текущий узел
- 
+
+            current.next = previous; // фиксируем текущий узел
+
             previous = current;
             current = next;
         }
- 
+
         // фиксируем голову, чтобы она указывала на новый фронт
         return previous;
     }
- 
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         // ключи ввода
         int[] keys = { 1, 2, 3, 4, 5, 6 };
- 
+
         Node head = null;
         for (int i = keys.length - 1; i >= 0; i--) {
             head = new Node(keys[i], head);
         }
- 
+
         head = reverse(head);
         printList(head);
     }
